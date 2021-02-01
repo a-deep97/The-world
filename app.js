793f363be809ec1c6d498e4f9fb1aa3ccef188b8
a-deep97 <a.deep97@outlook.com>
2021-02-01 22:51:08 +0530

    local database connected

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express =require('express')
+const express = require('express')
+const mongoose = require('mongoose')
 
 const app= express()
+
+const DBurl='mongodb://127.0.0.1:27017/the_world'
+mongoose.connect(DBurl,{useNewUrlParser:true})
 
 app.set('view engine','ejs')
 
 app.use(express.static('public'))
 app.use('/css',express.static(__dirname+'public/css'))
 app.use('/img',express.static(__dirname+'public/img'))
+
+const db= mongoose.connection
+db.once('open',()=>{
+    console.log('data base connected:',DBurl);
+})
+
 
 app.get('/',(req,res)=>{
 
