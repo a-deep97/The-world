e9292e1f9ab2d392caf4e3b2983ec6a00cab0a5f
a-deep97 <a.deep97@outlook.com>
2021-02-01 14:41:02 +0530

    server serving home page

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express =require('express')
 
 const app= express()
 
+app.set('view engine','ejs')
 
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname+'public/css'))
+app.use('/img',express.static(__dirname+'public/img'))
 
 app.get('/',(req,res)=>{
 
-    res.send('hello world');
+    res.render('index');
 })
 
 
