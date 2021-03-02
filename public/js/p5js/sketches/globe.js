46f7945419b323ed32faa4f89906f2aa580bdb30
a-deep97 <a.deep97@outlook.com>
2021-03-02 20:24:54 +0530

    globe made rotating in home page

--- public/js/p5js/sketches/globe.js
+++ public/js/p5js/sketches/globe.js
@@ -2,20 +2,26 @@
 //parent container of globe canvas
 const globe_container=document.querySelector('#globe-container');
 
+//global var
+let globe_angle=0;
+//setup function
 function setup() {
     let canvas=createCanvas(700,700,WEBGL);
     canvas.parent(globe_container);
     canvas.elt.style.display="block";  
 }
-  
+//preloading assets
 function preload() {
     img = loadImage('../../../img/globe-day-texture-2k.JPG');
 }
-  
+//looped draw function
 function draw() {
+
     background(255,255,255,0);
     push();
+    rotateY(0.2*globe_angle);
     texture(img);
     sphere(300,100,100);
     pop();
+    globe_angle+=0.01;
 }
