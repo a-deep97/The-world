713255e4575e02cd7d9a025ce8c4faea6d88c804
a-deep97 <a.deep97@outlook.com>
2021-03-11 00:49:26 +0530

    signup functionality defined with temporary local storage of users , signup form updated and gitignore updated

--- routers/signup.js
+++ routers/signup.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router();
+const bcrypt = require('bcrypt')
+
+//getting temporary user variable where user creds stored(only development purpose)
+let USERS=require('../users');
 
 
 router.use(bodyParser.urlencoded({extended:true}));
@@ -11,6 +15,25 @@ router.get('/',(req,res)=>{
     res.render('signup-page');
 });
 
+//post route
+router.post('/',async (req,res)=>{
+   try{
+       //hashing user password
+       const hashedPassword = await bcrypt.hash(req.body.password,12);
+       USERS.push({
+           id: req.body.name,
+           name: req.body.name,
+           email: req.body.email,
+           password: hashedPassword
+       });
+       console.log(USERS);
+       console.log('signed up');
+       res.redirect('login')
+   } catch{
+       console.log('couldn\'t sign up ');
+       res.redirect('register');
+   }
+});
 
 //exporting route
 module.exports=router;
