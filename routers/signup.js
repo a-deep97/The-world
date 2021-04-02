0623731f186dd9c0084b7a6c0b76c17709287700
a-deep97 <a.deep97@outlook.com>
2021-04-03 00:48:54 +0530

    now checks if user already exists in database

--- routers/signup.js
+++ routers/signup.js
@@ -12,7 +12,8 @@ router.use(bodyParser.urlencoded({extended:true}));
 //main route
 router.get('/',(req,res)=>{
     if(!req.isAuthenticated()){
-        res.render('signup-page',{req:req});
+        let message='';
+        res.render('signup-page',{req:req,message});
     }
     else{
         res.redirect('/');
@@ -21,24 +22,32 @@ router.get('/',(req,res)=>{
 
 //post route
 router.post('/',async (req,res)=>{
-   try{
-       //hashing user password
-       const hashedPassword = await bcrypt.hash(req.body.password,12);
-       const newUser= new userM({
-           _id:req.body.first_name+req.body.last_name+req.body.email,
-           first_name: req.body.first_name,
-           last_name: req.body.last_name,
-           email: req.body.email,
-           password: hashedPassword
-       });
-       newUser.save();
-       console.log(USERS);
-       console.log('signed up');
-       res.redirect('login')
-   } catch{
-       console.log('couldn\'t sign up ');
-       res.redirect('register');
-   }
+
+    userM.findOne({email:req.body.email},(err,user)=>{
+        if(user!=null){
+            let message='user already exists!';
+            res.render('signup-page',{req:req,message});
+        }
+    });
+    try{
+        //hashing user password
+        const hashedPassword = await bcrypt.hash(req.body.password,12);
+        const newUser= new userM({
+            _id:req.body.first_name+req.body.last_name+req.body.email,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        newUser.save();
+        //console.log(newUser);
+        console.log('signed up');
+        res.redirect('login');
+    }catch(e){
+        console.log(e);
+        console.log('couldn\'t sign up ');
+        res.redirect('register');
+    }
 });
 
 //exporting route
