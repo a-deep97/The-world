10e189bc5f5a2b3b515c0633e5a605892533a235
a-deep97 <a.deep97@outlook.com>
2021-05-08 19:34:53 +0530

    country model updated with database

--- models/countryM.js
+++ models/countryM.js
@@ -25,13 +25,7 @@ const countrySchema = mongoose.Schema({
     land_area:{
         type:String
     },
-    population:{
-        type:String
-    },
     motto:{
-        type:String
-    },
-    national_anthem:{
         type:String
     },
     president:{
@@ -58,6 +52,65 @@ const countrySchema = mongoose.Schema({
     capital_coordinates:{
         type: Array,
         default: [0,0]
+    },
+    water:{
+        type: String
+    },
+    estimate_population:{
+        type: String
+    },
+    last_census_population:{
+        type: String
+    },
+    GDP_PPP:{
+        type: Array,
+        default: [0,0]
+    },
+    GDP_nominal:{
+        type: Array,
+        default: [0,0]
+    },
+    driving_side:{
+        type: String
+    },
+    calling_code:{
+        type: String
+    },
+    population_density:{
+        type: String
+    },
+    demonym:{
+        type: String
+    },
+    gini:{
+        type: String
+    },
+    HDI:{
+        type: String
+    },
+    religion:{
+        type: String
+    },
+    membership:{
+        type: String
+    },
+    official_languages:{
+        type: String
+    },
+    formation:{
+        type: String
+    },
+    ethinic_groups:{
+        type: String
+    },
+    national_anthem_local:{
+        type: String
+    },
+    national_anthem_english:{
+        type: String
+    },
+    national_song:{
+        type:String
     }
 })
 
